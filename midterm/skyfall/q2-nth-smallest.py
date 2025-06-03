e33db4cbf09d29cdadc0ae84a161127b131c0f0d
Pony <pony@unnotech.com>
2025-06-03 21:17:33 +0800

    (feat dummy): add dummy changes to trigger git conflict

---
+++ midterm/skyfall/q2-nth-smallest.py
@@ -0,0 +1,11 @@
+# 咿呀哈
+# Please Keep this line
+lst = [2, 2, 3, 5, 6, 7, 9]
+
+N = 4
+
+lst_sort = sorted(list(set(lst)))
+if N > len(lst_sort):
+    print(f"沒有第{N}小的數字")
+else:
+    print(lst_sort[N-1])
