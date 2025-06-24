5a516bf499134ceb2fdd1bd8832bd30121ad2e5b
Boris <boris0939299008@gmail.com>
2025-06-24 09:22:14 +0800

    feat(docker-compose-practice): docker compose practice boris

--- docker-compose-practice/boris/example.py
+++ docker-compose-practice/boris/example.py
@@ -17,8 +17,5 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": f"redis://{os.getenv('REDIS_HOST', 'redis')}:{os.getenv('REDIS_PORT', '6379')}/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            }
     }
 }
